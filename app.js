9592e3436c314f4700b6212489931c926e09ac05
sammeastman <89807258+sammeastman@users.noreply.github.com>
2021-09-13 13:44:33 -0500

    i scrapped it last second, determined to make it perf

---
+++ app.js
@@ -0,0 +1,27 @@
+let cells = document.querySelectorAll(".row > div");
+let turnCount = 0;
+
+for (let i = 0; i < cells.length; i++) {
+  cells[i].addEventListener("click", cellClicked);
+}
+
+function cellClicked(e) {
+  if(turnCount % 2 == 0) {
+  e.target.textContent = 'X';
+} else {
+  e.target.textcontent = "O";
+}
+
+turnCount++
+  checkWin();
+}
+
+function checkWin() {
+  if (cells[0].textContent == cells[1].textContent && cells[1].textContent == cells[2].textContent) {
+      alert("EX WIN")
+  } else if (cells[3].textContent == cells[4].textContent && cells[4].textContent == cells[5].textContent) {
+    alert("I GUESS YOU WIN")
+  }
+}
+
+
