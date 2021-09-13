684334529b9c79ab44cbcae08f47bd8a3cedbf48
sammeastman <89807258+sammeastman@users.noreply.github.com>
2021-09-13 14:24:01 -0500

    plz help

--- app.js
+++ app.js
@@ -1,5 +1,17 @@
 let cells = document.querySelectorAll(".row > div");
 let turnCount = 0;
+let wind = cell [
+
+[cells[0], cells [1], cells [2]],
+[cells[3], cells [4], cells [5]],
+[cells[6], cells [7], cells [8]],
+[cells[0], cells [3], cells [6]],
+[cells[1], cells [4], cells [7]],
+[cells[2], cells [5], cells [8]],
+[cells[0], cells [4], cells [8]],
+[cells[2], cells [4], cells [6]],
+
+
 
 for (let i = 0; i < cells.length; i++) {
   cells[i].addEventListener("click", cellClicked);
@@ -17,20 +29,39 @@ turnCount++
 }
 
 function checkWin() {
-  if (cells[0].textContent == cells[1].textContent && cells[1].textContent == cells[2].textContent) {
-      alert("EX WIN")
-  } else if (cells[3].textContent == cells[4].textContent && cells[4].textContent == cells[5].textContent) {
-    alert("I GUESS YOU WIN")
-  } else if (cells[6].textContent == cells[7].textContent && cells[7].textContent == cells[8].textContent) {
-    alert("I GUESS YOU WIN")
-}  else if (cells[0].textContent == cells[3].textContent && cells[3].textContent == cells[6].textContent) {
-  alert("I GUESS YOU WIN")}
-  else if (cells[1].textContent == cells[4].textContent && cells[4].textContent == cells[7].textContent) {
-    alert("I GUESS YOU WIN")}
-    else if (cells[2].textContent == cells[5].textContent && cells[5].textContent == cells[8].textContent) {
-      alert("I GUESS YOU WIN")}
-      else if (cells[0].textContent == cells[4].textContent && cells[4].textContent == cells[8].textContent) {
-        alert("I GUESS YOU WIN")}
-        else if (cells[3].textContent == cells[4].textContent && cells[4].textContent == cells[5].textContent) {
-          alert("I GUESS YOU WIN")}
+for (let i = 0; i < wins.length; i++) {
+let xCount = 0;
+let oCount = 0;
+  
+for (let j = 0; j < wins[i].length; j++) {
+  if (cells[i].textContent == "X") {
+    xCount++
+  } else if (cells[i].textContent == "O") {
+    oCount++
   }
+}
+
+if (xCount == 3) {
+  alert("X Wins!");
+} else if (oCount == 3) {
+
+}
+  }
+}
+  //   if (cells[0].textContent == cells[1].textContent && cells[1].textContent == cells[2].textContent) {
+//       alert("EX WIN")
+//   } else if (cells[3].textContent == cells[4].textContent && cells[4].textContent == cells[5].textContent) {
+//     alert("I GUESS YOU WIN")
+//   } else if (cells[6].textContent == cells[7].textContent && cells[7].textContent == cells[8].textContent) {
+//     alert("I GUESS YOU WIN")
+// }  else if (cells[0].textContent == cells[3].textContent && cells[3].textContent == cells[6].textContent) {
+//   alert("I GUESS YOU WIN")}
+//   else if (cells[1].textContent == cells[4].textContent && cells[4].textContent == cells[7].textContent) {
+//     alert("I GUESS YOU WIN")}
+//     else if (cells[2].textContent == cells[5].textContent && cells[5].textContent == cells[8].textContent) {
+//       alert("I GUESS YOU WIN")}
+//       else if (cells[0].textContent == cells[4].textContent && cells[4].textContent == cells[8].textContent) {
+//         alert("I GUESS YOU WIN")}
+//         else if (cells[3].textContent == cells[4].textContent && cells[4].textContent == cells[5].textContent) {
+//           alert("I GUESS YOU WIN")}
+//   }
