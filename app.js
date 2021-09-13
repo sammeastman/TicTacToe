5ae114ba62bccd0a5823e5a7200d9e8cd34bd7cd
sammeastman <89807258+sammeastman@users.noreply.github.com>
2021-09-13 13:51:25 -0500

    plz help

--- app.js
+++ app.js
@@ -21,7 +21,16 @@ function checkWin() {
       alert("EX WIN")
   } else if (cells[3].textContent == cells[4].textContent && cells[4].textContent == cells[5].textContent) {
     alert("I GUESS YOU WIN")
+  } else if (cells[6].textContent == cells[7].textContent && cells[7].textContent == cells[8].textContent) {
+    alert("I GUESS YOU WIN")
+}  else if (cells[0].textContent == cells[3].textContent && cells[3].textContent == cells[6].textContent) {
+  alert("I GUESS YOU WIN")}
+  else if (cells[1].textContent == cells[4].textContent && cells[4].textContent == cells[7].textContent) {
+    alert("I GUESS YOU WIN")}
+    else if (cells[2].textContent == cells[5].textContent && cells[5].textContent == cells[8].textContent) {
+      alert("I GUESS YOU WIN")}
+      else if (cells[0].textContent == cells[4].textContent && cells[4].textContent == cells[8].textContent) {
+        alert("I GUESS YOU WIN")}
+        else if (cells[3].textContent == cells[4].textContent && cells[4].textContent == cells[5].textContent) {
+          alert("I GUESS YOU WIN")}
   }
-}
-
-
